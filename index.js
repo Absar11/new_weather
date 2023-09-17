c1ca32f0a8abe002b0beac73ecbd6dc6eb0d0f67
Absar <siddiqui.mr10@gmail.com>
2023-09-18 00:15:20 +0530

    starting new project

---
+++ index.js
@@ -0,0 +1,15 @@
+
+console.log("Before API");
+const API_KEY = "b4a539a2c10b21d0558410ab70cb1903";
+console.log("After API");
+
+async function showWeather(){
+    let city = 'goa';
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    console.log("Weather data:-> " , data);
+
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} ℃`;
+    document.body.appendChild(newPara);
+}
